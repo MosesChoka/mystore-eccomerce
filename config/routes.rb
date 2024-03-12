c1917114c12689130827497b4eced9538a6c545b
MC <chokamoses2@gmail.com>
2024-03-12 15:05:50 +0300

    create admin authentication and categories

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  devise_for :admins
+  root 'pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  #
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "/admin", to: "admin#index"
+end
